fa78ac0d4c48ab3f8334ca953a990562c4bd6cd6
bsamrendrakumar <bsamrendrakumar@gmail.com>
2024-11-28 23:17:54 +0530

    usin operators

---
+++ 01_class/02_opertators.py
@@ -0,0 +1,17 @@
+#operators
+a=20
+b=10
+print("sum is ",a+b)
+print("difference is ",a-b)
+print("multification is ",a*b)
+print("division is ",a/b)
+print("modulo is ",a%b)
+print(a==b)
+print(a!=b)
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+print(a!=b)
+print(a>b and b<a)
+print(a>b or a<b)
