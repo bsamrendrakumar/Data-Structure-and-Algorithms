3b1ad758746ac9119c73c5827d280bc4a1d8c24b
bsamrendrakumar <bsamrendrakumar@gmail.com>
2025-01-16 21:44:18 +0530

    add some file

---
+++ 02_class/02_type_casting.py
@@ -0,0 +1,18 @@
+#there are two typs of type casting
+# [1] Emplicit Type Casting
+'''
+a=10
+print(a)
+print(type(a))
+b=2.5
+c=a+b
+print(c)
+print(type(c))'''
+
+# [2] Explicit Type Casting
+A=10
+print(A)
+print("A type =",type(A))
+B=float(A)
+print(B)
+print("A type =",type(B))
