133989e0175c65c2024edeade24a4d49d51a088d
bsamrendrakumar <bsamrendrakumar@gmail.com>
2025-01-18 18:04:08 +0530

    add some files

---
+++ 02_class/05_sum_of_two_number.py
@@ -0,0 +1,4 @@
+a=int(input("Enter a first Numar :- "))
+b=int(input("Enter a second Numar :- "))
+c=a+b
+print("total sum is = ",c)
---
+++ 02_class/06_unary_operators.py
@@ -0,0 +1,33 @@
+# Unary Operator there are two types
+
+# (1) -> Increment operator 
+# a++ means ->> Post Increment operator [Kaam , Update]
+# ++a means ->> Pre Increment operator [Update , Kaam]
+
+# (2) -> Decrement operator -->> a
+# a-- means ->> Post Decrement operator [Kaam , Update]
+# --a means ->> Pre Decrement operator [Update , Kaam]
+
+print("(1) -> Increment operator")
+
+a = 10
+b = a+1
+print("a = ",a)
+print("b = ",b)
+
+a = 10
+b = 1+a
+print("a = ",a)
+print("b = ",b)
+
+print("(2) -> Decrement operator")
+
+c = 10
+d = c-1
+print("c = ",c)
+print("d = ",d)
+
+c = 10
+d = 1-c
+print("c = ",c)
+print("d = ",d)
